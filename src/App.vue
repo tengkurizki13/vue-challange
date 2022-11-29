7101dd3c45e7c6ba8e0ffd5b0b9c74c9470e2b22
Afwa Bagas Wahuda <bagas@skyshi.com>
2022-11-29 12:58:35 +0700

    add poppins

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap");
+
 #app {
+  font-family: "Poppins", sans-serif;
   text-align: center;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="hello">
     <!-- insert logo devcode on img element -->
-    <!-- <img alt="Devcode logo" /> -->
+    <img alt="Devcode logo" src="../assets/devcode-logo.png" />
     <div data-cy="hello-world-title">
       <!-- insert header element here -->
+      <h1>Welcome to Devcode!</h1>
     </div>
   </div>
 </template>
