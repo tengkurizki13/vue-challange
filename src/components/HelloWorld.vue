840669384aaabd1e04897e9a439189ae3f894079
Afwa Bagas Wahuda <bagas@skyshi.com>
2022-11-29 11:02:23 +0700

    add devcode logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
-  <div data-cy="hello-world-title" class="hello">
-    <!-- insert header element here -->
+  <div class="hello">
+    <!-- insert logo devcode on img element -->
+    <!-- <img alt="Devcode logo" /> -->
+    <div data-cy="hello-world-title">
+      <!-- insert header element here -->
+    </div>
   </div>
 </template>
 
@@ -13,6 +17,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .hello {
-  margin: 25% 0 0;
+  margin: 15% 0 0;
+
+  img {
+    width: 15%;
+    height: 15%;
+  }
 }
 </style>
